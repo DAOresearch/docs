c5337f0a389e5f47282ca24ec874fbe5db517e9d
burrrata <42473928+burrrata@users.noreply.github.com>
2020-05-16 15:50:47 -0400

    fixed config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@ module.exports = {
   url: 'https://daoresearch.github.io',
   baseUrl: '/daoshop/',
   favicon: 'img/favicon.ico',
-  organizationName: 'daoresearch', // Usually your GitHub org/user name.
+  organizationName: 'daoresear-ch', // Usually your GitHub org/user name.
   projectName: 'daoshop', // Usually your repo name.
   themeConfig: {
     navbar: {
